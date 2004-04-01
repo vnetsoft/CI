28bde92dc28d1cae6eb92143cf897a33905daa41
vnetsoft <vnetsoft.hyd@gmail.com>
2004-04-02 00:22:28 +0530

    commiting the changes

---
+++ myfirstprjt/src/main/java/com/citi_java/prjt1/App.java
@@ -0,0 +1,13 @@
+package com.citi_java.prjt1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
